333f8d9b5d7ccd73b2f62a9121defb75b72c7a9e
Ernesto Zarate <ernesto.zarate.bravo@gmail.com>
2020-08-01 02:14:11 -0700

    Add web api

---
+++ FoodDeliveryApp.Web.Api/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDeliveryApp.Models;
+using FoodDeliveryApp.Web.Managers.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoodDeliveryApp.Web.Api.Controllers
+{
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly IBaseManager<Menu> _manager;
+
+        public MenuController(IBaseManager<Menu> manager)
+        {
+            _manager = manager;
+        }
+
+        [Route("api/menus")]
+        [HttpGet]
+        public IEnumerable<Menu> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        [Route("api/menus/{id}")]
+        public Menu Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+    }
+}
---
+++ FoodDeliveryApp.Web.Api/Controllers/RestaurantController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDeliveryApp.Models;
+using FoodDeliveryApp.Web.Managers.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FoodDeliveryApp.Web.Api.Controllers
+{
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IBaseManager<Restaurant> _manager;
+
+        public RestaurantController(IBaseManager<Restaurant> manager)
+        {
+            _manager = manager;
+        }
+
+        [Route("api/restaurants")]
+        [HttpGet]
+        public IEnumerable<Restaurant> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        [Route("api/restaurant/{id}")]
+        [HttpGet]
+        public Restaurant Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+    }
+}
